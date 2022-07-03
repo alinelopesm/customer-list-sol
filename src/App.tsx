27cb394e825a12799626a5a06a6fea73456ffd66
alinelopesm <alinealopesm@gmail.com>
2022-07-03 11:59:42 -0300

    feat: Adiciona chamada inicial api pelo axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 function App() {
+    const [ user, setUser ] = useState({ name: '', login: '' });
+    
+    useEffect(() => {
+        api
+            .get('/users/romulo27')
+            .then((response) => {
+                console.log('Quem sou', response.data);
+                setUser(response.data);
+            })
+            .catch((err) => {
+                console.error('ops! ocorreu um erro' + err);
+            });
+    }, [user]);
+      
     return (
         <div>
             Energia limpa
-            User Aline git config user.name
+            User: {user ? user.name : ''}
         </div>
     );
 }
