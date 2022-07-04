4fd55b82545511120dc56cb64dbeb832c5c2d074
alinelopesm <alinealopesm@gmail.com>
2022-07-04 19:50:05 -0300

    refactor: Modifica rota inicial

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import App from './pages/App.tsx';
 import Financing from './pages/Financing';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
 
 export default function AppRouter() {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<App />} />
+                <Route path='/' element={<Main />} />
                 <Route path='/financing' element={<Financing />} />
             </Routes>
         </Router>
