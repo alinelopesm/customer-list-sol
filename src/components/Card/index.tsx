73dfc9dc7892367f23160e1c19fd1cad30b29e62
alinelopesm <alinealopesm@gmail.com>
2022-07-04 19:47:14 -0300

    feat: Adiciona infos card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,54 @@
+import './index.css';
+import { cessao } from '../../__mocks__/mock-list-full-formalizations';
+
 function Card() {
     return (
         <>
-        Sou Card
+            <h3>Total Formalizações</h3>
+            <div className="flex-container">
+                <div className="flex-items">
+                    <p>valor Financiado</p>
+                    <div>
+                        <span>PF R$ </span>
+                        <span>{cessao.pf.financiado}</span>
+                    </div>
+                    <div>
+                        <span>PJ R$ </span>
+                        <span>{cessao.pj.financiado}</span>
+                    </div>
+                    <div>
+                        <h3>Total R$ {cessao.total.financiado}</h3>
+                    </div>
+                </div>
+                <div className="flex-items">
+                    <p>valor Bruto</p>
+                    <div>
+                        <span>PF R$ </span>
+                        <span>{cessao.pf.bruto}</span>
+                    </div>
+                    <div>
+                        <span>PJ R$ </span>
+                        <span>{cessao.pj.bruto}</span>
+                    </div>
+                    <div>
+                        <h3>Total R$ {cessao.total.bruto}</h3>
+                    </div>
+                </div>
+                <div className="flex-items">
+                    <p>valor Aquisição</p>
+                    <div>
+                        <span>PF R$ </span>
+                        <span>{cessao.pf.aquisicao}</span>
+                    </div>
+                    <div>
+                        <span>PJ R$ </span>
+                        <span>{cessao.pj.aquisicao}</span>
+                    </div>
+                    <div>
+                        <h3>Total R$ {cessao.total.aquisicao}</h3>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
