feffe5c64c19b8633862fffcd90a8d9a69f85bcc
alinelopesm <alinealopesm@gmail.com>
2022-07-04 19:47:40 -0300

    feat: Adiciona infos header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
+import './index.css';
+
 function Header() {
     return (
-        <>
-        Sou header
-        </>
+        <div className="flex-container">
+            <div className="flex-items"><button className='btn'></button> </div>
+            <div className="flex-items"><h2>Formalizações</h2>Aprovadas - 05/01/2021 - 13:40:32</div>
+            <div className="flex-items"><button>Gerar CNAB</button></div>
+        </div>
     );
 }
 
